e946e2468b2fb40427e5b4c501394f1b86f5531c
kristynlyncheski <klyncheski@gmail.com>
2016-04-17 18:42:13 -0400

    read me and scoreboard changes

--- todoforroomies/frontend/app/containers/ScoreBoard.js
+++ todoforroomies/frontend/app/containers/ScoreBoard.js
@@ -8,15 +8,18 @@ const ScoreBoard = React.createClass ({
     return (
       <div className="pop-up-content">
         <h3>ScoreBoard</h3>
-        <div className="roommate-score-info">
-          <p className="scoreboard-roommate">Roommate 1: {this.props.roommate1name}</p>
-          <div>This week's score: {this.props.roommate1score}</div>
-        </div>
-        <div className="roommate-score-info">
-          <p className="scoreboard-roommate">Roommate 2: {this.props.roommate2name}</p>
-          <div>This week's score: {this.props.roommate2score}</div>
-        </div>
+        <div className="score-info-container">
 
+          <div className="roommate-score-info">
+            <p className="scoreboard-roommate">Roommate 1: {this.props.roommate1name}</p>
+            <div>This week's score: {this.props.roommate1score}</div>
+          </div>
+          <div className="roommate-score-info">
+            <p className="scoreboard-roommate">Roommate 2: {this.props.roommate2name}</p>
+            <div>This week's score: {this.props.roommate2score}</div>
+          </div>
+
+        </div>
         <Button onClick={this.props.closeBtn}>Close</Button>
       </div>
     )
